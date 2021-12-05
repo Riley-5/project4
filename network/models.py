7594787a212848a3c2ce35f187dc2b9841a538a4
Riley-5 <riley.kamstra@gmail.com>
2021-12-05 17:32:20 +0200

    Created model class for Posts

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# Posts class
+class Post(models.Model):
+    content = models.TextField()
+    dateTime = models.DateTimeField(auto_now = False, auto_now_add = True)
+    like = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "user")
